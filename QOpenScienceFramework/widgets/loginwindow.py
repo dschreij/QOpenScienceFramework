9d227ce34ecbe53d1ce52bbe9f8be409c3637c17
Daniel Schreij <dschreij@gmail.com>
2019-05-02 09:44:27 +0200

    Incremental project loading

--- QOpenScienceFramework/widgets/loginwindow.py
+++ QOpenScienceFramework/widgets/loginwindow.py
@@ -1,120 +1,120 @@
 # -*- coding: utf-8 -*-
-
 # Python3 compatibility
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from QOpenScienceFramework import dirname
+from QOpenScienceFramework.compat import *
+import QOpenScienceFramework.connection as osf
+from qtpy import QtCore
+
 import os
 import logging
 logging.basicConfig(level=logging.INFO)
 
-# QT classes
-# Required QT classes
-from qtpy import QtCore
-
 # QtWebKit is dropped in favour of QtWebEngine from Qt 5.6 on
 try:
-	from qtpy.QtWebKit import QWebView as WebView
+    from qtpy.QtWebKit import QWebView as WebView
 except ImportError:
-	from qtpy.QtWebEngineWidgets import QWebEngineView as WebView
+    from qtpy.QtWebEngineWidgets import QWebEngineView as WebView
 
 # OSF connection interface
-import QOpenScienceFramework.connection as osf
 # Python 2 and 3 compatiblity settings
-from QOpenScienceFramework.compat import *
-from QOpenScienceFramework import dirname
 
 osf_logo_path = os.path.join(dirname, 'img/cos-white2.png')
 
 # Dummy function later to be replaced for translation
-_ = lambda s: s
+
+
+def _(s): return s
+
 
 class LoginWindow(WebView):
-	""" A Login window for the OSF """
-	
-	# Login event is emitted after successfull login
-	logged_in = QtCore.Signal()
-	""" Event fired when user successfully logged in. """
+    """ A Login window for the OSF """
 
-	def __init__(self, *args, **kwargs):
-		super(LoginWindow, self).__init__(*args, **kwargs)
+    # Login event is emitted after successfull login
+    logged_in = QtCore.Signal()
+    """ Event fired when user successfully logged in. """
 
-		try:
-			# Create Network Access Manager to listen to all outgoing
-			# HTTP requests. Necessary to work around the WebKit 'bug' which
-			# causes it drop url fragments, and thus the access_token that the
-			# OSF Oauth system returns
-			self.nam = self.page().networkAccessManager()
+    def __init__(self, *args, **kwargs):
+        super(LoginWindow, self).__init__(*args, **kwargs)
 
-			# Connect event that is fired if a HTTP request is completed.
-			self.nam.finished.connect(self.checkResponse)
-		except:
-			pass
-			# Connect event that is fired if a HTTP request is completed.
-			# self.finished.connect(self.checkResponse)
+        try:
+            # Create Network Access Manager to listen to all outgoing
+            # HTTP requests. Necessary to work around the WebKit 'bug' which
+            # causes it drop url fragments, and thus the access_token that the
+            # OSF Oauth system returns
+            self.nam = self.page().networkAccessManager()
 
-		# Connect event that is fired after an URL is changed
-		# (does not fire on 301 redirects, hence the requirement of the NAM)
-		self.urlChanged.connect(self.check_URL)
+            # Connect event that is fired if a HTTP request is completed.
+            self.nam.finished.connect(self.checkResponse)
+        except:
+            pass
+            # Connect event that is fired if a HTTP request is completed.
+            # self.finished.connect(self.checkResponse)
 
-	def checkResponse(self, reply):
-		"""Callback function. Do not use directly. 
+        # Connect event that is fired after an URL is changed
+        # (does not fire on 301 redirects, hence the requirement of the NAM)
+        self.urlChanged.connect(self.check_URL)
 
-		Callback for NetworkRequestManager.finished event
-		used to check if OAuth2 is redirecting to a link containing the token
-		string. This is necessary for the QtWebKit module, because it drops
-		fragments after being redirect to a different URL. QWebEngine uses the
-		check_URL function to check for the token fragment
+    def checkResponse(self, reply):
+        """Callback function. Do not use directly. 
 
-		Parameters
-		----------
-		reply : QtNetwork.QNetworkReply
-			The response object provided by NetworkRequestManager
-		"""
-		request = reply.request()
-		# Get the HTTP statuscode for this response
-		statuscode = reply.attribute(request.HttpStatusCodeAttribute)
-		# The accesstoken is given with a 302 statuscode to redirect
+        Callback for NetworkRequestManager.finished event
+        used to check if OAuth2 is redirecting to a link containing the token
+        string. This is necessary for the QtWebKit module, because it drops
+        fragments after being redirect to a different URL. QWebEngine uses the
+        check_URL function to check for the token fragment
 
-		# Stop if statuscode is not 302 (HTTP Redirect)
-		if statuscode != 302:
-			return
+        Parameters
+        ----------
+        reply : QtNetwork.QNetworkReply
+            The response object provided by NetworkRequestManager
+        """
+        request = reply.request()
+        # Get the HTTP statuscode for this response
+        statuscode = reply.attribute(request.HttpStatusCodeAttribute)
+        # The accesstoken is given with a 302 statuscode to redirect
 
-		redirectUrl = reply.attribute(request.RedirectionTargetAttribute)
-		if not redirectUrl.hasFragment():
-			return
+        # Stop if statuscode is not 302 (HTTP Redirect)
+        if statuscode != 302:
+            return
 
-		r_url = redirectUrl.toString()
-		if osf.settings['redirect_uri'] in r_url:
-			try:
-				self.token = osf.parse_token_from_url(r_url)
-			except ValueError as e:
-				logging.warning(e)
-			else:
-				self.logged_in.emit()
-				self.hide()
+        redirectUrl = reply.attribute(request.RedirectionTargetAttribute)
+        if not redirectUrl.hasFragment():
+            return
 
-	def check_URL(self, url):
-		""" Callback function. Do not use directly.
-		
-		Calback for urlChanged event.
+        r_url = redirectUrl.toString()
+        if osf.settings['redirect_uri'] in r_url:
+            try:
+                self.token = osf.parse_token_from_url(r_url)
+            except ValueError as e:
+                logging.warning(e)
+            else:
+                self.logged_in.emit()
+                self.hide()
 
-		Parameters
-		----------
-		command : url
-			New url, provided by the urlChanged event
+    def check_URL(self, url):
+        """ Callback function. Do not use directly.
 
-		"""
-		url_string = url.toString()
+        Calback for urlChanged event.
 
-		# QWebEngineView receives token here.
-		if url.hasFragment():
-			try:
-				self.token = osf.parse_token_from_url(url_string)
-			except ValueError as e:
-				logging.warning(e)
-			else:
-				self.logged_in.emit()
-				self.hide()
+        Parameters
+        ----------
+        command : url
+            New url, provided by the urlChanged event
+
+        """
+        url_string = url.toString()
+
+        # QWebEngineView receives token here.
+        if url.hasFragment():
+            try:
+                self.token = osf.parse_token_from_url(url_string)
+            except ValueError as e:
+                logging.warning(e)
+            else:
+                self.logged_in.emit()
+                self.hide()
--- example.py
+++ example.py
@@ -7,10 +7,18 @@ You should have received a copy of the Apache v2.0 License
 along with this module. If not, see <http://www.apache.org/licenses/>.
 """
 # Python3 compatibility
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+from QOpenScienceFramework.compat import *
+from QOpenScienceFramework.manager import ConnectionManager
+from QOpenScienceFramework import connection as osf
+from QOpenScienceFramework import widgets, events
+from qtpy import QtWidgets, QtCore
+from dotenv import load_dotenv
 
 # Import basics
 import sys
@@ -18,118 +26,116 @@ import os
 import logging
 import tempfile
 logging.basicConfig(level=logging.INFO)
-from dotenv import load_dotenv
 load_dotenv()
 
-# Required QT classes
-from qtpy import QtWidgets, QtCore
-# Widgets
-from QOpenScienceFramework import widgets, events
-from QOpenScienceFramework import connection as osf
-# Event dispatcher and listeners
-from QOpenScienceFramework.manager import ConnectionManager
-from QOpenScienceFramework.compat import *
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
-####### CONFIGURE THE CLIENT ID AND REDIRECT URI HERE. REGISTER AT OSF.IO ######
-client_id = os.getenv("CLIENT_ID", "<YOUR_CLIENT_ID_HERE>")
-redirect_uri = os.getenv("REDIRECT_URI", "<YOUR_REDIRECT_URI_HERE>")
-################################################################################
+# Required QT classes
+# Widgets
+# Event dispatcher and listeners
+
+# CONFIGURE THE CLIENT ID AND REDIRECT URI HERE. REGISTER AT OSF.IO
+# You can set the parameters here, or place them as environment variables in a .env file
+client_id = os.getenv("OSF_CLIENT_ID", "<YOUR_CLIENT_ID_HERE>")
+redirect_uri = os.getenv("OSF_REDIRECT_URI", "<YOUR_REDIRECT_URI_HERE>")
+
 
 class InvalidateButton(QtWidgets.QWidget):
-	""" Just a button to tamper with the OSF session and see what the app does
-	to recover from missing authentication information """
+    """ Just a button to tamper with the OSF session and see what the app does
+    to recover from missing authentication information """
 
-	def __init__(self, *args, **kwargs):
-		super(InvalidateButton, self).__init__(*args, **kwargs)
-		self.setLayout(QtWidgets.QHBoxLayout())
-		pb = QtWidgets.QPushButton("Invalidate session")
-		pb.clicked.connect(self.invalidate_session)
-		self.layout().addWidget(pb)
+    def __init__(self, *args, **kwargs):
+        super(InvalidateButton, self).__init__(*args, **kwargs)
+        self.setLayout(QtWidgets.QHBoxLayout())
+        pb = QtWidgets.QPushButton("Invalidate session")
+        pb.clicked.connect(self.invalidate_session)
+        self.layout().addWidget(pb)
 
-	def invalidate_session(self):
-		print("Invalidating session!")
-		osf.session = osf.create_session()
-		print(osf.session.token)
+    def invalidate_session(self):
+        print("Invalidating session!")
+        osf.session = osf.create_session()
+        print(osf.session.token)
+
 
 class StandAlone(object):
-	""" Class that opens all available widgets when instantiated for testing
-	purposes. """
+    """ Class that opens all available widgets when instantiated for testing
+    purposes. """
 
-	def __init__(self):
-		# Check if client_id and redirect_uri have been changed
-		if client_id == "<YOUR_CLIENT_ID_HERE>":
-			raise RuntimeError("Please insert the client_id you have registered"
-				" for your app at the OSF")
-		if redirect_uri == "<YOUR_REDIRECT_URI_HERE>":
-			raise RuntimeError("Please insert the redirect uri you have registered"
-				" for your app at the OSF")
+    def __init__(self):
+        # Check if client_id and redirect_uri have been changed
+        if client_id == "<YOUR_CLIENT_ID_HERE>":
+            raise RuntimeError("Please insert the client_id you have registered"
+                               " for your app at the OSF")
+        if redirect_uri == "<YOUR_REDIRECT_URI_HERE>":
+            raise RuntimeError("Please insert the redirect uri you have registered"
+                               " for your app at the OSF")
 
-		# Set OSF server settings
-		server_settings = {
-		 	"client_id"		: client_id,
-			"redirect_uri"	: redirect_uri,
-		}
-		# Add these settings to the general settings
-		osf.settings.update(server_settings)
-		osf.create_session()
+        # Set OSF server settings
+        server_settings = {
+            "client_id"		: client_id,
+            "redirect_uri"	: redirect_uri,
+        }
+        # Add these settings to the general settings
+        osf.settings.update(server_settings)
+        osf.create_session()
 
-		tmp_dir = safe_decode(tempfile.gettempdir())
-		tokenfile = os.path.join(tmp_dir, u"osf_token.json")
-		# Create manager object
-		self.manager = ConnectionManager(tokenfile=tokenfile)
+        tmp_dir = safe_decode(tempfile.gettempdir())
+        tokenfile = os.path.join(tmp_dir, u"osf_token.json")
+        # Create manager object
+        self.manager = ConnectionManager(tokenfile=tokenfile)
 
-		# Init and set up user badge
-		self.user_badge = widgets.UserBadge(self.manager)
-		self.user_badge.move(850, 100)
+        # Init and set up user badge
+        self.user_badge = widgets.UserBadge(self.manager)
+        self.user_badge.move(850, 100)
 
-		# Set-up project tree
-		project_tree = widgets.ProjectTree(self.manager, use_theme="Faenza")
+        # Set-up project tree
+        project_tree = widgets.ProjectTree(self.manager, use_theme="Faenza")
 
-		# Init and set up Project explorer
-		self.project_explorer = widgets.OSFExplorer(
-			self.manager, tree_widget=project_tree
-		)
-		self.project_explorer.move(50, 100)
+        # Init and set up Project explorer
+        self.project_explorer = widgets.OSFExplorer(
+            self.manager, tree_widget=project_tree
+        )
+        self.project_explorer.move(50, 100)
 
-		# Token file listener writes the token to a json file if it receives
-		# a logged_in event and removes this file after logout
-		# Filename of the file to store token information in.
-		self.tfl = events.TokenFileListener(tokenfile)
+        # Token file listener writes the token to a json file if it receives
+        # a logged_in event and removes this file after logout
+        # Filename of the file to store token information in.
+        self.tfl = events.TokenFileListener(tokenfile)
 
-		self.manager.dispatcher.add_listeners(
-			[
-				self.manager, self.tfl, project_tree,
-				self.user_badge, self.project_explorer
-			]
-		)
-		# Connect click on user badge logout button to osf logout action
-		self.user_badge.logout_request.connect(self.manager.logout)
-		self.user_badge.login_request.connect(self.manager.login)
+        self.manager.dispatcher.add_listeners(
+            [
+                self.manager, self.tfl, project_tree,
+                self.user_badge, self.project_explorer
+            ]
+        )
+        # Connect click on user badge logout button to osf logout action
+        self.user_badge.logout_request.connect(self.manager.logout)
+        self.user_badge.login_request.connect(self.manager.login)
 
-		# self.ib = InvalidateButton()
-		# self.ib.setGeometry(850,200,200,50)
-		# self.ib.show()
+        # self.ib = InvalidateButton()
+        # self.ib.setGeometry(850,200,200,50)
+        # self.ib.show()
 
-		# If a valid token is stored in token.json, use that.
-		# Otherwise show the login window.
-		self.manager.login()
-		# Show the user badge
-		self.user_badge.show()
-		self.project_explorer.show()
+        # If a valid token is stored in token.json, use that.
+        # Otherwise show the login window.
+        self.manager.login()
+        # Show the user badge
+        self.user_badge.show()
+        self.project_explorer.show()
+
 
 if __name__ == "__main__":
-	app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
-	print("Using Qt {}".format(QtCore.PYQT_VERSION_STR))
+    print("Using Qt {}".format(QtCore.PYQT_VERSION_STR))
 
-	# Enable High DPI display with PyQt5
-	if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-		app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
+    # Enable High DPI display with PyQt5
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 
-	test = StandAlone()
-	exitcode = app.exec_()
-	logging.info("App exiting with code {}".format(exitcode))
-	sys.exit(exitcode)
-
-
-
+    test = StandAlone()
+    exitcode = app.exec_()
+    logging.info("App exiting with code {}".format(exitcode))
+    sys.exit(exitcode)
