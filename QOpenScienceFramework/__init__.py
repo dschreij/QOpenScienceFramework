c1b0c548530ad58dedd252ff8412047434a7b824
smathot <s.mathot@cogsci.nl>
2020-11-30 15:21:36 +0100

    Bump to 1.3.2

--- QOpenScienceFramework/__init__.py
+++ QOpenScienceFramework/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __author__ = "Daniel Schreij"
 import os
 import sys
@@ -8,6 +8,3 @@ dirname = safe_decode(os.path.dirname(__file__), enc=sys.getfilesystemencoding()
 import QOpenScienceFramework.manager
 import QOpenScienceFramework.connection
 import QOpenScienceFramework.widgets
-
-
-
